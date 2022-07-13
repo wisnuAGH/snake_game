49b44f74895493ad01942d4e7cd62fb52315ff4c
wisnuAGH <mwisniewski@student.agh.edu.pl>
2022-07-13 08:12:42 +0200

    new: - working restart button - working settings button - settings window

--- main.py
+++ main.py
@@ -22,17 +22,17 @@ class Snake:
             self.coordinates.append([0, 0])
 
         for x, y in self.coordinates:
-            square = canvas.create_rectangle(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill = SNAKE_COLOR, tag="snake")
+            square = canvas.create_rectangle(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill = SNAKE_COLOR, tags="snake")
             self.squares.append(square)
 
 class Food:
     def __init__(self):
-        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
-        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+        x = random.randint(0, int(GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, int(GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
 
         self.coordinates = [x, y]
 
-        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tags="food")
 
 def next_move(snake, food):
 
@@ -104,19 +104,78 @@ def check_collisions(snake):
     return False
 
 def game_over():
-    global game_over
+    global gameover
     canvas.delete(ALL)
     canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2,
-                       font=('ds-digital', 70), text="GAME OVER", fill="red", tag="gameover")
-    R = Button(window, text="Play again", font=('ds-digital', 50), width=10, bg="gray",
-               fg="green", activeforeground="green")
+                       font=('ds-digital', 70), text="GAME OVER", fill="red", tags="gameover")
+    R = Button(window, text="Play again", command=restart, font=('ds-digital', 50), width=10, bg="gray",
+               fg="green", activeforeground="green")   # PLAY AGAIN button
     canvas.create_window(600, 700,  window=R)
 
-    B = Button(window, text="settings", font=('ds-digital', 30), width=10, bg="gray", fg="black")
-    canvas.create_window(1000, 700,  window=B)
+    S = Button(window, text="settings", command=settings, font=('ds-digital', 30), width=10, bg="gray", fg="black")
+    canvas.create_window(1000, 700,  window=S)     # SETTINGS button
 
-    game_over = True
+    gameover = True
 
+def restart():
+    canvas.delete(ALL)
+    main()
+
+def settings():
+    canvas.delete(ALL)
+    label.config(text="Settings")   # Settings window
+
+    # TEXT
+    canvas.create_text(100, 100, anchor=tkinter.NW, font=('ds-digital', 40),
+                       text="speed: {}".format(SPEED), fill="grey")
+    canvas.create_text(100, 200, anchor=tkinter.NW, font=('ds-digital', 40),
+                       text="body parts: {}".format(BODY_PARTS), fill="grey")
+    canvas.create_text(100, 300, anchor=tkinter.NW, font=('ds-digital', 40),
+                       text="space size: {}".format(SPACE_SIZE), fill="grey")
+    canvas.create_text(100, 400, anchor=tkinter.NW, font=('ds-digital', 40),
+                       text="none", fill="grey")
+
+    # Entry Boxes
+    entry_speed = tkinter.Entry(window)
+    canvas.create_window(canvas.winfo_width()/2, 125, window=entry_speed)
+    entry_body_parts = tkinter.Entry(window)
+    canvas.create_window(canvas.winfo_width() / 2, 225, window=entry_body_parts)
+    entry_space_size = tkinter.Entry(window)
+    canvas.create_window(canvas.winfo_width() / 2, 325, window=entry_space_size)
+
+    # Save function
+    def save():
+        if entry_speed.get() != 0:
+            SPEED = entry_speed.get()
+        if entry_speed.get() != 0:
+            BODY_PARTS = entry_body_parts.get()
+        if entry_speed.get() != 0:
+            SPACE_SIZE = entry_space_size.get()
+        canvas.update()
+
+    # Buttons
+    R = Button(window, text="Play again", command=restart, font=('ds-digital', 50), width=10, bg="gray",
+               fg="green", activeforeground="green")  # PLAY AGAIN button
+    canvas.create_window(600, 700, window=R)
+    Save = Button(window, text="SAVE", command=save, font=('ds-digital', 30), width=10, bg="gray", fg="black")
+    canvas.create_window(1000, 700, window=Save)  # SAVE button
+
+
+def main():
+    global score
+    score = 0
+
+    label.config(text="score:{}".format(score))
+    label.pack()
+    canvas.pack()
+    window.update()
+
+    snake = Snake()
+    food = Food()
+
+    next_move(snake, food)
+
+    window.mainloop()
 
 window = Tk()
 window.title("The Snake")
@@ -127,7 +186,6 @@ direction = 'down'
 
 label = Label(window, text="Score:{}".format(score), font=('ds-digital', 50))
 label.pack()
-
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
 
@@ -138,8 +196,8 @@ window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
-x = int((screen_width/2) - (window_width/2))
-y = int((screen_height/2) - (window_height/2))
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
 
 window.bind('<Left>', lambda event: change_direction('left'))
 window.bind('<Right>', lambda event: change_direction('right'))
@@ -154,3 +212,5 @@ food = Food()
 next_move(snake, food)
 
 window.mainloop()
+
+main()
